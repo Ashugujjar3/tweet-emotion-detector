98ee1b4ebd6cda72ee98b3cddf7a7944c5bc3c93
Ashugujjar3 <dayamaom350@gmail.com>
2025-09-25 22:57:36 +0530

    Update train_and_eval.py

--- src/train_and_eval.py
+++ src/train_and_eval.py
@@ -1,1 +1,9 @@
+from sklearn.metrics import classification_report
+import pandas as pd
+cr = classification_report(y_test, preds, output_dict=True)
+pd.DataFrame(cr).transpose().to_csv(os.path.join(OUTPUT_DIR, 'classification_report.csv'))
 
+# Save misclassified examples
+dfm = pd.DataFrame({'text': X_test, 'true': y_test, 'pred': preds})
+mis = dfm[dfm['true'] != dfm['pred']]
+mis.to_csv(os.path.join(OUTPUT_DIR, 'misclassified_examples.csv'), index=False)
